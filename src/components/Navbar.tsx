9b379f78054b5c536180fe62f2560662be91b23c
Kelly Coldren <shrtpvteacher@protonmail.com>
2025-06-26 17:51:08 -0700

    fixed padding in navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -21,7 +21,7 @@ const Navbar: React.FC = () => {
 
   return (
     <nav className="bg-white shadow-lg border-b border-gray-200 sticky top-0 z-50">
-  <div className="max-w-7xl mx-auto px-2 sm:px-2 lg:px-2">
+  <div className="max-w-7xl mx-auto px-4 sm:px-4 lg:px-4">
     <div className="flex items-center h-16">
       {/*  Left: logo + name */}
       <Link to="/" className="flex items-center gap-2 shrink-0">
