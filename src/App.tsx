d73cb6fd0d6593483810338ae1586cccfeedd576
Kelly Coldren <shrtpvteacher@protonmail.com>
2025-06-29 12:24:34 -0700

    temporarily removed  HowToMetaMask Route from App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -15,13 +15,12 @@ import SubmitForReview from './pages/SubmitForReviewPage';
 import VerifyDelivery from './pages/VerifyDeliveryPage';
 import SendToDropbox from './pages/SendToDropboxPage'; 
 import HowToPage from './pages/HowToPage';
-import HowToMetaMask from './pages/how-to/HowToMetaMask';
+
 import HowToFundAccount from './pages/how-to/HowToFundAccount';
 import HowToConnectWallet from './pages/how-to/HowToConnectWallet';
 import HowToPostJob from './pages/how-to/HowToPostJob';
 import HowToSubmitWork from './pages/how-to/HowToSubmitWork';
 import HowToAiNotApproved from './pages/how-to/HowToAiNotApproved';
-
 
 
 function App() {
@@ -44,7 +43,6 @@ function App() {
               <Route path="/verify-delivery/:contractAddress/:jobId" element={<VerifyDelivery />} />
               <Route path="/send-to-dropbox/:contractAddress" element={<SendToDropbox />} />
               <Route path="/how-to" element={<HowToPage />} />
-              <Route path="/how-to/metamask" element={<HowToMetaMask />} />
               <Route path="/how-to/fund-account" element={<HowToFundAccount />} />
               <Route path="/how-to/connect-wallet" element={<HowToConnectWallet />} />
               <Route path="/how-to/post-job" element={<HowToPostJob />} />
